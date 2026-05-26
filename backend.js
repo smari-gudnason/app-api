7306a67a527a191d95267931a7d3fd58e251dbd0
Smári Guðnason <smari@explorer.is>
2026-05-26 12:55:21 +0000

    debug: log db url

--- backend.js
+++ backend.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
 const express = require('express');
 const { Pool } = require('pg');
 const path = require('path');
